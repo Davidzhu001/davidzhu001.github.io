2af74f1b4e5bd2774204355dd8b2ba86fffebdbd
Weicheng Zhu <weicheng.zhu@icloud.com>
2024-12-05 00:21:59 -0500

    修改一些个人的github信息

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -52,16 +52,16 @@ module.exports = {
     social:{ // 社交图标，显示于博主信息栏和页脚栏
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
       icons: [
-        // {
-        //   iconClass: 'icon-youjian',
-        //   title: '发邮件',
-        //   link: 'mailto:oddfar@163.com'
-        // },
-        // {
-        //   iconClass: 'icon-github',
-        //   title: 'GitHub',
-        //   link: 'https://github.com/oddfar'
-        // },
+        {
+          iconClass: 'icon-youjian',
+          title: '发邮件',
+          link: 'mailto:weicheng.zhu@icloud.com'
+        },
+        {
+          iconClass: 'icon-github',
+          title: 'GitHub',
+          link: 'https://github.com/davidzhu001'
+        },
       ]
     },
     footer: { // 页脚信息
