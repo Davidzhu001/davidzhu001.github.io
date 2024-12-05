aa09845423460d85519361b2548e3f6ef4956a30
Weicheng Zhu <weicheng.zhu@icloud.com>
2024-12-04 23:59:51 -0500

    重新移植

---
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -0,0 +1,12 @@
+module.exports = [
+  { text: 'Profile', link: '/' },
+  {
+    text: 'Unreal', link: '/company/'
+  },
+  {
+    text: 'Backend', link: '/solution/'
+  },
+  { text: 'Mobile', link: '/about/' },
+  { text: 'Desktop', link: '/spider/' },
+  { text: 'Web', link: '/spider/' }
+]
